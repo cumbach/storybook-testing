ad31ba120e1a62e974c7bcfc3f442035d6dd3187
Chris Umbach <chrisumbach@gmail.com>
2020-12-16 16:53:11 -0800

    start tutorial

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Example from './Example.js';
 
--- src/Example.js
+++ src/Example.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './Example.css';
 import logo from './logo.svg';
 
